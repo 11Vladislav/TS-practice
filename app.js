954935477197a1f6d639609ad1bbacf987e2e733
Vladislav <91179551+11Vladislav@users.noreply.github.com>
2023-02-05 22:26:29 +0200

    second task

--- app.js
+++ app.js
@@ -7,3 +7,68 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+var age;
+var name;
+var toggle;
+var empty;
+var notInitialize;
+var callback = function (a) { return number; };
+age = 50;
+name = 'Max';
+toggle = true;
+empty = null;
+notInitialize = undefined;
+callback = function (a) { return 100 + a; };
+var anything;
+anything = 'Text';
+anything = {};
+var some;
+some = 'Text';
+var str;
+if (typeof some === 'string') {
+    str = some;
+}
+var person;
+person = ['Max', 21];
+var union;
+var literal;
+var Load;
+(function (Load) {
+    Load[Load["LOADING"] = 0] = "LOADING";
+    Load[Load["READY"] = 1] = "READY";
+})(Load || (Load = {}));
+;
+var page = {
+    load: Load.READY
+};
+if (page.load === Load.LOADING) {
+    console.log('Page loading');
+}
+if (page.load === Load.READY) {
+    console.log('Page loaded');
+}
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error('Error');
+}
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: '2021-01-01',
+        updateAt: '2021-05-01'
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close'
+};
